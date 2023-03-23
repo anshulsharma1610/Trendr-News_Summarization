c33cd6809ae90f65aa56ad89c431e360fc2ba9fc
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-03-23 04:27:30 -0400

    added password hash using bcrypt

--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     fname: String,
@@ -14,6 +15,25 @@ const userSchema = new mongoose.Schema({
     image: {
         type: String,
     }
+});
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 const Users = mongoose.model('User', userSchema);
