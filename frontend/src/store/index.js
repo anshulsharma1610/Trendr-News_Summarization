6df4fb39fb0c270ea114cca3ba41d7855092f4e6
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-04-12 17:01:18 -0400

    added service worker

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import customizationReducer from './reducers/customizationReducer';
-
+import userReducer from './reducers/userReducer';
 
 const store = configureStore({
     reducer: {
         // Add reducers here
         customization: customizationReducer,
-        // user: userReducer
+        user: userReducer
     }
 });
 
