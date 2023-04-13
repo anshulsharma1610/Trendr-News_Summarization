ae4434ff214c06720eecb7ce50854090cfe8239c
ShubhiMiradwalNEU <miradwal.s@northeastern.edu>
2023-04-13 00:22:45 -0400

    trending news feed

--- frontend/src/views/TrendingNews.js
+++ frontend/src/views/TrendingNews.js
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import MainCard from 'components/cards/MainCard';
-// import { getNews } from '../../../backend/api/controllers/fetchNews-controller.js';
-
+import { getTopNews } from './fetch.js'; // import the getTopNews function from fetch.js
 
 const TrendingNews = () => {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
     const fetchNewsData = async () => {
-      const newsData = await getNews(); // call getNews to fetch the news data
+      const newsData = await getTopNews(); // call getTopNews to fetch the news data
       setNews(newsData);
     };
-
     fetchNewsData();
   }, []);
 
   return (
     <MainCard>
       <h1>Headlines</h1>
-      {/* display the news data here */}
+      <ul>
+        {news.map((item) => (
+          <div >
+          <li key={item.link}>
+            <a href={item.link}>
+              <h2>{item.title}</h2>
+            </a>
+            <img src={item.image_url} alt={item.title} />
+            <h2>{item.pubDate}</h2>
+          </li>
+          </div>
+        ))}
+      </ul>
     </MainCard>
   );
 };
 
 export default TrendingNews;
-
