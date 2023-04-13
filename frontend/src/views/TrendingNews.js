948892dd71e0bd138dbac7e53c11e645d1080e74
ShubhiMiradwalNEU <miradwal.s@northeastern.edu>
2023-04-13 13:29:38 -0400

    Updated scss

--- frontend/src/views/TrendingNews.js
+++ frontend/src/views/TrendingNews.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import MainCard from 'components/cards/MainCard';
-import { getTopNews } from './fetch.js'; // import the getTopNews function from fetch.js
+import { getTopNews } from './fetch.js';
+import { Typography, List, ListItem, Link, Grid } from '@mui/material';
+import './trendingNews.scss';
+
 
 const TrendingNews = () => {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
     const fetchNewsData = async () => {
-      const newsData = await getTopNews(); // call getTopNews to fetch the news data
+      const newsData = await getTopNews();
       setNews(newsData);
     };
     fetchNewsData();
@@ -15,20 +18,40 @@ const TrendingNews = () => {
 
   return (
     <MainCard>
-      <h1>Headlines</h1>
-      <ul>
-        {news.map((item) => (
-          <div >
-          <li key={item.link}>
-            <a href={item.link}>
-              <h2>{item.title}</h2>
-            </a>
-            <img src={item.image_url} alt={item.title} />
-            <h2>{item.pubDate}</h2>
-          </li>
-          </div>
-        ))}
-      </ul>
+      <div>
+        <Typography variant="h4" gutterBottom>
+          Headlines
+        </Typography>
+        <List>
+          {news.map((item) => (
+            <ListItem key={item.link}>
+              <Link href={item.link}>
+                <Grid container alignItems="center">
+                  <Grid item>
+                    <img
+                      src={item.image_url}
+                      alt={item.title}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Typography
+                      variant="h6"
+                      noWrap
+                    >
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                    >
+                      {item.pubDate}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
+      </div>
     </MainCard>
   );
 };
