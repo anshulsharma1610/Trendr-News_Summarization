2331430c2af9bc81426dd2e2501b50d77fb40ee4
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-04-16 17:04:31 -0400

    added validation

--- backend/api/models/userSubscriptons.js
+++ backend/api/models/userSubscriptons.js
@@ -4,18 +4,16 @@ const userSubscriptionsSchema = new mongoose.Schema({
     userId: String,
     subId: String,
     price: Number,
-    createdAt: {
-        type: Date,
-        // default: Date.now
-    },
+    createdAt: Date,
     validTill: Date
 });
 
 // before save calculate validTill date from current date and incoming parameter tenureDays
 userSubscriptionsSchema.pre('save', function (next) {
-    // this.validTill = new Date().setDate(new Date().getDate() + 30);
-    // console.log('===valid', this.validTill)
-    // this.validTill = new Date(currentDate.setDate(currentDate.getDate() + this.tenureDays));
+    if (!this.createdAt || this.validTill) {
+        this.createdAt = new Date();
+        this.validTill = new Date().setDate(new Date().getDate() + 30);
+    }
     next();
 });
 
