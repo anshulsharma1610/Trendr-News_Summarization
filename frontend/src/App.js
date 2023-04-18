d1a895a91a186b76de1897fac473a0f3af110940
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-04-18 17:42:56 -0400

    updated trending news ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import * as React from 'react';
 import { useSelector } from 'react-redux';
 
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 // routing
 import Routes from 'routes';
@@ -9,18 +14,48 @@ import Routes from 'routes';
 // defaultTheme
 import themes from 'themes';
 
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 // ==============================|| APP ||============================== //
 
 const App = () => {
     const customization = useSelector((state) => state.customization);
+    const [state, setState] = React.useState({
+        open: true,
+        vertical: 'top',
+        horizontal: 'center',
+    });
+    const { vertical, horizontal, open } = state;
+
+    const handleClick = () => {
+        setState({ open: true, ...newState });
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setState({ ...state, open: false });
+    };
 
     return (
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={themes(customization)}>
                 <CssBaseline />
+                {/* <Button variant="outlined" onClick={handleClick}>
+                    Open success snackbar
+                </Button> */}
+                <Snackbar open={open} autoHideDuration={2000} onClose={handleClose} anchorOrigin={{ vertical, horizontal }}>
+                    <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                        This is a success message!
+                    </Alert>
+                </Snackbar>
                 <Routes />
             </ThemeProvider>
-        </StyledEngineProvider>
+        </StyledEngineProvider >
     );
 };
 
