884de65bacd1fbe9e66d8707d0fb9421aea3d842
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-04-19 18:46:18 -0400

    ui change

--- frontend/src/views/Checkout/SubscriptionPlans/PlansCard.js
+++ frontend/src/views/Checkout/SubscriptionPlans/PlansCard.js
@@ -7,42 +7,61 @@ import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Box } from "@mui/system";
-import logo from 'assets/images/icons/pro-logo.jpeg'
-
-
+import logo from 'assets/images/icons/pro-logo.jpeg';
 
 function StripePayment({ product, makePayment }) {
-
     return (
-        <Card sx={{ width: 300, margin: '5px', textAlign: 'center', paddingTop:'20px' , paddingRight:'20px' , maxWidth:300}}>
+        <Card sx={{
+            width: 300,
+            margin: '5px',
+            paddingTop: '20px',
+            paddingRight: '20px',
+            maxWidth: 300
+        }}>
             <CardMedia
                 component="img"
                 height="194"
                 image={logo}
                 alt="product image"
             />
-            <CardContent  style={{ fontSize:'20px'}} >
-                <Typography  style={{ fontSize:'15px', textAlign: 'center'}} gutterBottom variant="h4" component="div">
-                    <b>{product.name}</b>
+            <CardContent>
+                <Typography
+                    gutterBottom
+                    variant="h2"
+                    component="div"
+                    justifyContent='center'
+                    textAlign="center"
+                >
+                    <b>{product.title}</b>
                 </Typography>
-                <Typography style={{ fontSize:'15px', textAlign: 'center'}} variant="body2" color="text.secondary">
+                <Typography
+                    variant="body2"
+                    color="text.primary"
+                >
                     <b>{product.desc}</b>
                 </Typography>
-                <Typography style={{ fontSize:'15px', textAlign: 'center', justifyContent: 'center'}} variant="body2" color="text.secondary" component="ul">
+                <Typography
+                    variant="body2"
+                    color="text.primary"
+                    component="ul"
+                >
                     {product.features.map((feature, index) => (
-                        <li  key={index}> <b>{feature}</b></li>
+                        <li key={index}> <b>{feature}</b></li>
                     ))}
                 </Typography>
                 <Box sx={{ my: 3 }} />
-                <Typography variant="body2" color="text.secondary">
-                    <b>Price: ${product.price}</b>
+                <Typography variant="h4" color="text.primary">
+                    <b>${product.price} for {product.tenureDays} days</b>
                 </Typography>
             </CardContent>
             <CardActions sx={{ justifyContent: 'center' }}>
-                <Button size="large" variant="contained" onClick={() => makePayment(product)}>
+                <Button
+                    size="large"
+                    variant="contained"
+                    onClick={() => makePayment(product)}
+                >
                     Pay Now
                 </Button>
-
             </CardActions>
         </Card>
     );
