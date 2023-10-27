4be08745f3f5234d2d209b3cdfaf3571e4fa0659
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-10-27 19:40:24 -0400

    env modif

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -5,7 +5,7 @@ import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 
-const API_URL = process.env.AUTH_API_URL || "http://localhost:8000/auth/";
+const API_URL = process.env.REACT_APP_AUTH_API_URL || "http://localhost:8000/auth/";
 
 const register = (fname, lname, email, password) => {
     return axios.post(API_URL + "signup", {
